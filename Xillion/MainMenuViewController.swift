e27a72e67d2e3b88f8ffe32d9854c9010c2221be
Robinson Merillat <rdmerillat@mymail.mines.edu>
2016-11-21 02:40:05 -0700

    beginning steps
    began to integrate UI HUD systems and import a game map.

---
+++ Xillion/MainMenuViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainMenuViewController.swift
+//  Xillion
+//
+//  Created by Robbie Merillat on 11/21/16.
+//  Copyright © 2016 Robbie Merillat. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    @IBOutlet weak var inProgressImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
