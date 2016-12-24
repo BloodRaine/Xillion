588a344427502741427ab1f1b233178ddd0c0640
Robinson Merillat <rdmerillat@mymail.mines.edu>
2016-12-23 21:11:03 -0700

    creating new classes

---
+++ Xillion/Monster.swift
@@ -0,0 +1,9 @@
+//
+//  Monster.swift
+//  Xillion
+//
+//  Created by Robbie Merillat on 12/23/16.
+//  Copyright © 2016 Robbie Merillat. All rights reserved.
+//
+
+import Foundation
---
+++ Xillion/weapon.swift
@@ -0,0 +1,9 @@
+//
+//  weapon.swift
+//  Xillion
+//
+//  Created by Robbie Merillat on 12/23/16.
+//  Copyright © 2016 Robbie Merillat. All rights reserved.
+//
+
+import Foundation
